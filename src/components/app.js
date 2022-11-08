208308b9990804bbbfd6febf9bb957675f7ae3ac
Hemanth Krishna <hkpdev008@gmail.com>
2022-11-09 02:14:14 +0530

    add: mobile check

--- src/components/app.js
+++ src/components/app.js
@@ -1,17 +1,27 @@
 import { h } from "preact";
 import { Router } from "preact-router";
-import '@vidstack/player/hydrate.js';
+import "@vidstack/player/hydrate.js";
 
 import Home from "../routes/home";
 import Mason from "../routes/mason";
+import Mobile from "./mobile";
 
-const App = () => (
-  <div id="app">
-    <Router>
-      <Home path="/" />
-      <Mason path="/:id/" id="me" />
-    </Router>
-  </div>
-);
+const App = () => {
+  const isMobile =
+    Math.min(window.screen.width, window.screen.height) < 768 ||
+    navigator.userAgent.indexOf("Mobi") > -1;
+  return (
+    <div id="app">
+      {isMobile ? (
+        Mobile()
+      ) : (
+        <Router>
+          <Home path="/" />
+          <Mason path="/:id/" id="me" />
+        </Router>
+      )}
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/mobile.js
@@ -0,0 +1,13 @@
+const Mobile = () => {
+  return (
+    <div className="bg-gray-800 h-screen flex w-full items-center justify-center bg-grey-lighter">
+      <label className="w-64 flex flex-col items-center px-4 py-6 bg-white text-blue rounded-lg shadow-lg tracking-wide text-center border border-blue cursor-pointer hover:bg-gray-800 hover:text-blue-500">
+        <span className="mt-2 text-base leading-normal">
+          Sorry 😔 <br />
+          B.Y.O.S isn't available for mobile devices yet! <br />
+        </span>
+      </label>
+    </div>
+  );
+};
+export default Mobile;
